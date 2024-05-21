de927fc243ce3814b690d1e7d64d198961a73faa
vish201280 <vish.201280@gmail.com>
2024-05-21 09:21:48 +0100

    com-nopcommerce-vishal-Homework

---
+++ src/ComNopCommerce.java
@@ -0,0 +1,62 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ComNopCommerce {
+    public static void main(String[] args) throws InterruptedException {
+        String baseurl = "https://demo.nopcommerce.com/";
+
+        // 1. Setup Chrome browser.
+        WebDriver driver = new ChromeDriver();
+
+        //2. open the url
+        driver.get(baseurl);
+
+        //3. Print the title of the page
+        System.out.println("The title of the page is   :" + driver.getTitle());
+
+        //4. Print the current url.
+        System.out.println("The URL of the page is  :" + driver.getCurrentUrl());
+
+        //5. Print the page source
+        System.out.println("Page source:  " + driver.getPageSource());
+
+        //6. Navigate to URL
+        String loginUrl = "https://demo.nopcommerce.com/login?returnUrl=%2F";
+        driver.navigate().to(loginUrl);
+
+        //7. Print the current URL
+        System.out.println("Get navigated URL : " + driver.getCurrentUrl());
+
+        //8. Navigate back to the home page.
+        driver.navigate().back();
+        System.out.println("Get new navigated URL : " + driver.getCurrentUrl());
+
+        //9. Navigate to the login page.
+        driver.navigate().forward();
+        System.out.println("Navigation to login page:  " + driver.getCurrentUrl());
+
+        //10. Print the current url.
+        System.out.println("Current URL : " + driver.getCurrentUrl());
+
+        //11. Refresh the page.
+        driver.navigate().refresh();
+
+        // 12. Enter the email to email field
+        WebElement email = driver.findElement(By.id("Email"));
+        email.sendKeys("prime123@gmail.com");
+
+        // 13. Enter the password to password field
+        WebElement passwordBox = driver.findElement(By.id("Password"));
+        passwordBox.sendKeys("Prime1");
+
+        // 14. Click on Login Button
+        WebElement loginButton = driver.findElement(By.xpath("//button[contains(text(),'Log in')]"));
+        loginButton.click();
+
+        //15. Close the browser
+        driver.quit();
+
+    }
+}
